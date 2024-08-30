7672ce37ae1fac8a2e161679b8211285b085e37a
numdo <110229694+numdo@users.noreply.github.com>
2024-08-30 22:13:34 +0900

    [Gold V] Title: 사과나무, Time: 240 ms, Memory: 23556 KB -BaekjoonHub

--- 백준/Gold/19539. 사과나무/사과나무.java
+++ 백준/Gold/19539. 사과나무/사과나무.java
@@ -1,29 +1,28 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int total = 0;
-		int one = 0;
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			total += arr[i];
-			one += arr[i]%2; //높이를 2로 나눴을 때 나머지가 1이면 oneCount 증가
-		}
-		if(total %3 ==0 && one <=total/3) {
-			System.out.println("YES");
-		}
-		else {
-			System.out.println("NO");
-		}
-		
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int sum = 0; // 높이의 총합
+        int twos = 0; // 필요한 2의 개수
+        
+        for (int i = 0; i < N; i++) {
+            int h = Integer.parseInt(st.nextToken());
+            sum += h;
+            twos += h / 2; // 각 나무의 높이 h를 2로 나누어 나올 수 있는 2의 개수
+        }
 
+        // 조건 1: 높이의 총합이 3의 배수인가?
+        // 조건 2: twos가 sum / 3 보다 크거나 같은가?
+        if (sum % 3 == 0 && twos >= sum / 3) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
