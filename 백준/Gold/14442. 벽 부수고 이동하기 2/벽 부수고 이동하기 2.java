dfd02b18fe7e20c8393bb8128dcdafe97eb97acc
numdo <110229694+numdo@users.noreply.github.com>
2024-11-13 00:37:13 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1152 ms, Memory: 332972 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,76 +1,60 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-
-	static int N,M,K;
+	static int N, M, K;
 	static int[][] map;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	public static void main(String[] args) throws Exception{
+	static boolean[][][] visited;
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		for(int i=0;i<N;i++) {
+		visited = new boolean[K + 1][N][M];
+		for (int i = 0; i < N; i++) {
 			String line = br.readLine();
-			for(int j=0;j<M;j++) {
+			for (int j = 0; j < M; j++) {
 				map[i][j] = line.charAt(j) - '0';
 			}
 		}
+		System.out.println(bfs());
 
-		System.out.println(bfs(0,0));
-		
 	}
 
-	public static int bfs(int x,int y) {
+	public static int bfs() {
 		Queue<int[]> q = new LinkedList<>();
-		boolean[][][] visited = new boolean[N][M][K+1];
-		
-		q.offer(new int[] {x,y,K});
-		visited[x][y][K] = true;
-		int count = 1;
-		
-		while(!q.isEmpty()) {
-			int size = q.size();
-			for(int i=0;i<size;i++) {
-				int[] cur = q.poll();
-				int cx = cur[0];
-				int cy = cur[1];
-				int wall = cur[2];
-				if(cx == N-1 && cy == M-1) {
-					return count;
-				}
-				for(int dir = 0;dir<4;dir++) {
-					int nx = cx + dx[dir];
-					int ny = cy + dy[dir];
-					
-					if(isMap(nx,ny)) {
-						if(map[nx][ny] == 0 && !visited[nx][ny][wall]) {
-							visited[nx][ny][wall] = true;
-							q.offer(new int[] {nx,ny,wall});
-						}
-						else if(map[nx][ny] == 1 && wall >0  && !visited[nx][ny][wall-1]) {
-							visited[nx][ny][wall-1] = true;
-							q.offer(new int[] {nx,ny,wall-1});
-						}
-					}
-					
+		q.add(new int[] { 0, 0, 0, 1 });
+		visited[0][0][0] = true;
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			if (cur[0] == N - 1 && cur[1] == M - 1) {
+				return cur[3];
+			}
+			for (int i = 0; i < 4; i++) {
+				int nx = cur[0] + dx[i];
+				int ny = cur[1] + dy[i];
+				if (!isIn(nx, ny) || visited[cur[2]][nx][ny])
+					continue;
+				if (map[nx][ny] == 0) {
+					visited[cur[2]][nx][ny] = true;
+					q.add(new int[] { nx, ny, cur[2], cur[3] + 1 });
+				} else if(map[nx][ny] == 1 && cur[2] < K && !visited[cur[2]+1][nx][ny]) {
+					visited[cur[2] + 1][nx][ny] = true;
+					q.add(new int[] { nx, ny, cur[2] + 1, cur[3] + 1 });
 				}
 			}
-			count++;
 		}
 		return -1;
-		
 	}
-	public static boolean isMap(int x,int y) {
-		return x>=0 && x<N && y>=0 && y<M;
+
+	public static boolean isIn(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < M;
 	}
-	
 }
