6d596c1f6f6c26cb58bb6291b18db506d12e5bff
numdo <110229694+numdo@users.noreply.github.com>
2025-05-10 23:32:57 +0900

    [level 1] Title: 모의고사, Time: 0.95 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,12 +1,10 @@
 import java.io.*;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer;
 
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
+        int[] scores = new int[3];
         
         int[] math1 = {1,2,3,4,5};
         int[] math2 = {2,1,2,3,2,4,2,5};
@@ -14,46 +12,26 @@ class Solution {
         
         for(int i=0;i<answers.length;i++){
             if(answers[i] == math1[i%math1.length]){
-                count1++;
+                scores[0]++;
             }
             if(answers[i] == math2[i%math2.length]){
-                count2++;
+                scores[1]++;
             }
             if(answers[i] == math3[i%math3.length]){
-                count3++;
+                scores[2]++;
             }
         }
-        System.out.println(count1);
-        System.out.println(count2);
-        System.out.println(count3);
-        if(count1 > count2 && count1 > count3){
-            answer = new int[1];
-            answer[0] = 1;
-        } else if(count2 > count1 && count2 > count3){
-            answer = new int[1];
-            answer[0] = 2;
-        } else if(count3 > count1 && count3 > count2){
-            answer = new int[1];
-            answer[0] = 3;
-        } else if(count1 == count2 && count2 > count3){
-            answer = new int[2];
-            answer[0] = 1;
-            answer[1] = 2;
-        } else if(count1 == count3 && count1 > count2){
-            answer = new int[2];
-            answer[0] = 1;
-            answer[1] = 3;
-        } else if(count2 == count3 && count2 > count1){
-            answer = new int[2];
-            answer[0] = 2;
-            answer[1] = 3;
-        } else{
-            answer = new int[3];
-            answer[0] = 1;
-            answer[1] = 2;
-            answer[2] = 3;
+        int maxScore = Math.max(scores[0],Math.max(scores[1], scores[2]));
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<3;i++){
+            if(scores[i] == maxScore){
+                list.add(i+1);
+            }
         }
-        
+        answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
